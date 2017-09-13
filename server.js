d25c6b8842730a1f2c43c512fa48a878c3e8ebce
hersh <neu_lhx@163.com>
2017-09-13 19:29:17 +0800

    9.13提交

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var catchFirstUrl = 'http://www.tianyancha.com/search/',
     startDate = new Date(),
     endDate = null,
     pageCount = 0,
-    pageNum = 5;
+    pageNum = 10;
 
 var proxy = 'http://114.215.103.121:8081';
 
@@ -46,7 +46,6 @@ function unique(val, arr) {
 }
 
 var firstTask = function (cb) {
-  console.log(pageUrls[pageCount])
   superagent.get(pageUrls[pageCount])
       .end(function (err, pres) {
         console.log('fetch ' + pageUrls[pageCount] + ' successful');
@@ -65,8 +64,6 @@ var firstTask = function (cb) {
 };
 
 var restTask = function (pageCount, cb) {
-  console.log(pageUrls[pageCount])
-  console.log('---------------')
   superagent.get(pageUrls[pageCount])
       .end(function (err, pres) {
         console.log('fetch ' + pageUrls[pageCount] + ' successful');
@@ -96,8 +93,74 @@ async.waterfall(tasks, function (err, result) {
   if (err) {
     console.error(err)
   }
-  console.log('0000000000000000000000000000')
-  console.log(infoUrls)
+  console.log('-----------------------------------------------------------------------------------')
+  var concurrencyCount = 0;
+
+  var fetchInfo = function (url, callback) {
+    var delay = parseInt((Math.random() * 10000000) % 2000, 10);
+    concurrencyCount++;
+    console.log('现在的并发数是', concurrencyCount, '，正在抓取的是', url, '，耗时' + delay + '毫秒');
+
+    superagent.get(url)
+        .end(function (err, sres) {
+          // 常规的错误处理
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          var $ = cheerio.load(sres.text);
+
+          var nameWrapper = $('.f18.in-block.vertival-middle');
+          var telWrapper = $('.in-block.vertical-top.overflow-width.mr20').eq(0).children().eq(1);
+          var emailWrapper = $('.in-block.vertical-top.overflow-width.emailWidth').eq(0);
+          var hrefWrapper = $('.in-block.vertical-top.overflow-width.mr20').eq(1).children().eq(1);
+          var addressWrapper = $('.in-block.vertical-top.overflow-width.emailWidth').eq(1);
+
+          var name = nameWrapper.text()
+          var tel = telWrapper.text()
+          var email = emailWrapper.text()
+          var href = hrefWrapper.text()
+          var address = addressWrapper.text()
+
+          var row = [];
+          row.push(name);
+          row.push(tel);
+          row.push(email);
+          row.push(href);
+          row.push(address);
+
+          unique(row, infoArray);
+        });
+
+    setTimeout(function () {
+      concurrencyCount--;
+      callback(null, url + ' Call back content');
+    }, delay);
+  };
+
+  async.mapLimit(infoUrls, 1, function (url, callback) {
+    fetchInfo(url, callback)
+  }, function (err, result) {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('final:');
+      console.log(result);
+      console.log(infoArray);
+      endDate = new Date();
+      infoArray.unshift(['公司名', '电话', '邮箱', '网址', '地址'])
+      var buffer = xlsx.build([{name: "companyInfo", data: infoArray}]);
+      fs.writeFileSync('./file/company_info.xlsx', buffer);
+      console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+      console.log('爬虫统计结果')
+      console.log('1、爬虫开始时间：' + startDate)
+      console.log('2、爬虫结束时间：' + endDate)
+      console.log('3、耗时：' + (endDate - startDate) + 'ms' + ' --> ' + (Math.round((endDate - startDate) / 1000 / 60 * 100) / 100) + 'min')
+      console.log('4、爬虫遍历的企业数目：' + (infoArray.length - 1))
+      console.log('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    }
+  })
 });
 
 // ep.after('FetchContent', pageUrls.length * 20, function (infoUrls) {
@@ -170,23 +233,3 @@ async.waterfall(tasks, function (err, result) {
 //     }
 //   })
 // });
-
-// pageUrls.forEach(function (pageUrl) {
-//   superagent.get(pageUrl)
-//       .end(function (err, pres) {
-//         console.log('fetch ' + pageUrl + ' successful');
-//         if (err) {
-//           console.error(err);
-//         }
-//
-//         var $ = cheerio.load(pres.text);
-//         var curPageUrls = $('.query_name');
-//         for (var i = 0; i < curPageUrls.length; i++) {
-//           var infoUrl = curPageUrls.eq(i).attr('href');
-//           ep.emit('FetchContent', infoUrl);
-//         }
-//       })
-// });
-
-
-
